4cfffd84650d89c6f9b47676b39ff0f5547f8d24
jimjodj <59851792+jimjodj@users.noreply.github.com>
2020-01-15 11:22:45 +0000

    Add files via upload

---
+++ WebApplication2/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using WebApplication1.ServiceReference1;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //ServiceReference1. webService = new WebService();
+            string FileName = "C:\\Users\\Jimmy\\source\\repos\\WebApplication2\\WebApplication1\\XMLDataTest.XML";
+            string dataXML = System.IO.File.ReadAllText(FileName);
+            XDocument x = XDocument.Load(FileName);
+            WebService1 ws = new WebService1();
+            int i = ws.ParseXML(x);
+            
+            //webService.
+        }
+
+}
+}
---
+++ WebApplication2/WebApplication1/WebService1.asmx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+namespace WebApplication1
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int ParseXML(XDocument XMLMsg)
+        {
+            int returnValue=0;
+            
+
+            XmlSchemaSet settings = new XmlSchemaSet();
+            
+            settings.Add(null, @"C:\Users\Jimmy\source\repos\WebApplication2\WebApplication1\XMLDataTest1.XSD");
+           
+            XMLMsg.Validate(settings, (o, e) =>
+            {
+
+                returnValue = -3;
+            }); 
+            if (returnValue == -3) { return returnValue; }
+            IEnumerable<XElement> Defaults = from e in XMLMsg.Descendants("Declaration")
+                                         where (string)e.Attribute("Command") == "DEFAULT"
+                                         select e;
+
+            if (Defaults.Count()==0)
+            {
+                returnValue = - 1;
+            };
+            IEnumerable<XElement> Dubs= from e in XMLMsg.Descendants("SiteID")
+                                         where (string)e.Value == "DUB"
+                                         select e;
+            if (Dubs.Count()==0)
+            {
+                returnValue = - 2;
+            };
+
+            return returnValue;
+        }
+    }
+}
+
